ffe9f42fca774696c248a6ba40fa6fb9574fb2d9
abhimaurya910 <140160434+abhimaurya910@users.noreply.github.com>
2024-01-10 20:09:31 +0530

    checkin

--- 01-starting-project/src/Components/NewTask.jsx
+++ 01-starting-project/src/Components/NewTask.jsx
@@ -18,12 +18,12 @@ export default function ({onAdd}){
           
     }
     return(
-        <div className="flex items-center gap-4">
-            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        <div className=" flex items-center gap-4">
+            <input type=" text" className=" w-64 px-2 py-1 rounded-sm bg-stone-200"
             onChange={hnadleChange}
             value={enteredTask}
             />
-            <button className="text-stone-700 hover:text-stone-950"
+            <button className=" text-stone-700 hover:text-stone-950"
             onClick={handleClick}
             >Add task</button>
         </div>
--- 01-starting-project/src/Components/Task.jsx
+++ 01-starting-project/src/Components/Task.jsx
@@ -4,14 +4,14 @@ export default function Task({ tasks,onAdd, onDelete}){
 
     return(
         <section>
-            <h1 className="text-2xl font-bold text-stone-700 mb-4">TASK</h1>
+            <h1 className=" text-2xl font-bold text-stone-700 mb-4">TASK</h1>
             <NewTask onAdd={onAdd}/>
-            {tasks.length===0&&<p className="text-stone-800 mb-4">This Projec didnot add task yet.</p>}
+            {tasks.length===0&&<p className=" text-stone-800 mb-4">This Projec didnot add task yet.</p>}
            {tasks.length>0&& (
-           <ul className="p-4 mt-8 rounded-md bg-ston-100">
+           <ul className=" p-4 mt-8 rounded-md bg-ston-100">
             {tasks.map((task)=>
              <li key={task.id} className="flex justify-between my-4"><span>{task.text}</span>
-            <button className="text-stone-700 hover:text-red-500"
+            <button className=" text-stone-700 hover:text-red-500"
             onClick={()=>onDelete(task.id)}>
                 Delete
                  </button>
